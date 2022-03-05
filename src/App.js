e26aefb1d442cfcf254282c249fbe70376de88e3
soladragon <johncraigmurphy@gmail.com>
2022-03-05 12:39:54 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HexGrid, Layout, Hexagon, Text, GridGenerator, HexUtils } from 'react-hexgrid';
+import configs from './configurations';
 import './App.css';
+import { hexes } from "./hexes.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const config = configs['hexagon'];
+    const generator = GridGenerator.getGenerator(config.map);
+    const hexagons = generator.apply(this, config.mapProps);
+    this.state = { hexagons, config };
+  }
+
+  changeType(event) {
+    const name = event.currentTarget.value;
+    const config = configs[name];
+    const generator = GridGenerator.getGenerator(config.map);
+    const hexagons = generator.apply(this, config.mapProps);
+    this.setState({ hexagons, config });
+  }
+
+  render() {
+    const { hexagons, config } = this.state;
+    const layout = config.layout;
+    const size = { x: layout.width, y: layout.height };
+    return (
+      <div className="App">
+        <h2>Select grid type and configuration from dropdown.</h2>
+        <div>
+          <strong>Template: </strong>
+          <select onChange={(ev) => this.changeType(ev)}>
+            {Object.keys(configs).map((type) => (
+              <option name={type}>{type}</option>
+            ))}
+          </select>
+        </div>
+        <hr />
+        <HexGrid width={config.width} height={config.height} width="100%">
+          <Layout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
+            {
+              // note: key must be unique between re-renders.
+              // using config.mapProps+i makes a new key when the goal template chnages.
+              hexes.map((hex, i) => (
+                <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s}>
+                  {/* <Text>{HexUtils.getID(hex)}</Text> */}
+                </Hexagon>
+              ))
+            }
+          </Layout>
+        </HexGrid>
+      </div>
+    );
+  }
 }
 
 export default App;
